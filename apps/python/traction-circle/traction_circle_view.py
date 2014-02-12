3d6808deb22269041a58beae643a10c42df4ddcb
psmith <tallpsmith@gmail.com>
2014-02-13 07:03:19 +1100

    First attempt at colour fading for olderdata points.

--- apps/python/traction-circle/traction_circle_view.py
+++ apps/python/traction-circle/traction_circle_view.py
@@ -2,11 +2,8 @@ import ac
 
 
 class TractionCircleView:
-    RED = 77.0/255
-    GREEN = 184.0/255
-    BLUE = 73.0 / 255
-
-    FINAL_COLOUR = {31,77,31}
+    START_COLOUR = {'red': 77.0, 'green': 184.0, 'blue': 73.0}
+    FINAL_COLOUR = {'red': 31.0, 'green': 77.0, 'blue': 31.0}
 
     def __init__(self, window, tractionCircleModel, gPlotter):
         self.WIDTH = 2.0
@@ -14,17 +11,42 @@ class TractionCircleView:
 
         self.gPlotter = gPlotter
         self.label = ac.addLabel(window, '')
-        ac.setPosition(self.label, 100, 100)
+        ac.setPosition(self.label, 160, 180)
 
         self.tractionCircleModel = tractionCircleModel
 
     def render(self):
         try:
-            ac.glColor3f(self.RED, self.GREEN, self.BLUE)
-            for dataPoint in self.tractionCircleModel.dataPoints():
+            dataPoints = self.tractionCircleModel.dataPoints()
+            colourFades = self.colourFader(dataPoints.size(), self.START_COLOUR, self.FINAL_COLOUR)
+
+            for dataPoint in dataPoints:
                 x, y = self.gPlotter.plotG(dataPoint['x'], dataPoint['z'])
+                ac.glColor3f(colourFades['red'], colourFades['green'], colourFades['blue'])
                 ac.glQuad(x, y, self.WIDTH, self.HEIGHT)
 
             ac.setText(self.label, "{0}".format(len(self.tractionCircleModel.dataPoints())))
         except Exception as e:
             ac.log(str(e))
+
+
+    def colourFader(self, numberOfSlots, startColour, endColour):
+        startColourRed = startColour['red']
+        endColourRed = endColour['red']
+        startColourGreen = startColour['green']
+        endColourGreen = endColour['green']
+        startColourBlue = startColour['blue']
+        endColourBlue = endColour['blue']
+        colourFadeRed = list(
+            (x for x in range(startColourRed, startColourRed, -round((startColourRed - endColourRed) / numberOfSlots))))
+        colourFadeGreen = list((x for x in range(startColourGreen, startColourGreen,
+                                                 -round((startColourGreen - endColourGreen) / numberOfSlots))))
+        colourFadeBlue = list((x for x in range(startColourBlue, startColourBlue,
+                                                -round((startColourBlue - endColourBlue) / numberOfSlots))))
+        colours = list()
+        for x in range(numberOfSlots):
+            colours.append(
+                {'red': colourFadeRed[x] / 255, 'green': colourFadeGreen[x] / 255, 'blue': colourFadeBlue[x]} / 255)
+
+        return colours
+
