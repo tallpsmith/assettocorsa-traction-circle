9bb2587fda4f03b95aba87eababa1045b061eb36
Paul Smith <psmith@aconex.com>
2014-02-14 09:10:35 +1100

    Added a plotAll method to the gPlotter so that we can produce something that can more easily create a sliding window

--- apps/python/traction-circle/traction_circle_g_plotter.py
+++ apps/python/traction-circle/traction_circle_g_plotter.py
@@ -5,6 +5,14 @@ class GPlotter():
         self.maxZRange = maxZRange
         self.maxXRange = maxXRange
 
+    def plotAll(self, dataPoints):
+        allPoints = []
+        for point in dataPoints:
+            allPoints.append(self.plotG(point['x'], point['z']))
+
+        return allPoints
+
+
     def plotG(self, xGValue, zGValue):
         # normalize the acceleration to the Maximum G size, and offset that from the center point
         xPoint = ((xGValue / self.maxXRange) * (0.5 * self.windowSizeX)) + (0.5 * self.windowSizeX)
--- apps/tests/test_GPlotter.py
+++ apps/tests/test_GPlotter.py
@@ -1,20 +1,32 @@
 from unittest import TestCase
+
 from traction_circle_g_plotter import GPlotter
 
-class TestGPlotter(TestCase):
-    def test_halfBreakingNoCornering(self):
-        plotter =  GPlotter(320,200, 2, 2)
 
-        x,y = plotter.plotG(0, 1)
+class TestGPlotter(TestCase):
+    def test_plotAll(self):
+        plotter = GPlotter(320, 200, 2, 2)
+        dataPoints = [{"x": 1, "z": 1}, {"x": 1, "z": 1}]
+
+        allPlots = plotter.plotAll(dataPoints)
+
+        self.assertEquals(2, len(allPlots))
+        self.assertTrue(allPlots[0][0] < 320 and allPlots[0][0] > 0,
+                        "Plotted x values must be between the range -160:160 " + str(allPlots))
+
+
+    def test_halfBreakingNoCornering(self):
+        plotter = GPlotter(320, 200, 2, 2)
+
+        x, y = plotter.plotG(0, 1)
 
         self.assertEquals(160, x)
         self.assertEquals(150, y)
 
     def test_centerPoint(self):
+        plotter = GPlotter(320, 200, 2, 2)
 
-        plotter =  GPlotter(320,200, 2, 2)
-
-        x,y = plotter.plotG(0, 0)
+        x, y = plotter.plotG(0, 0)
         self.assertEquals(160, x)
         self.assertEquals(100, y)
 
