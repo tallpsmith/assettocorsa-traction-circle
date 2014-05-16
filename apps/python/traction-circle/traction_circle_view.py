09feb0c61126b133c031b50ce47441b2375a42d2
Bruno Gonzalez <stenyak@stenyak.com>
2014-05-16 22:04:07 +0100

    Add colour fade to moving average line

--- apps/python/traction-circle/traction_circle_view.py
+++ apps/python/traction-circle/traction_circle_view.py
@@ -5,15 +5,18 @@ from moving_average_plotter import MovingAveragePlotter
 
 
 class TractionCircleView:
-    FINAL_COLOUR = {'red': 77, 'green': 184, 'blue': 77}
-    START_COLOUR = {'red': 31, 'green': 77, 'blue': 31}
+    FINAL_COLOUR_DATA_POINTS = {'red': 77, 'green': 184, 'blue': 77}
+    START_COLOUR_DATA_POINTS = {'red': 31, 'green': 77, 'blue': 31}
+    FINAL_COLOUR_MOVING_AVERAGE = {'red': 255, 'green': 255, 'blue': 255}
+    START_COLOUR_MOVING_AVERAGE = {'red': 64, 'green': 64, 'blue': 64}
 
     def __init__(self, window, tractionCircleModel, gPlotter, movingAvgPlotter):
         self.WIDTH = 2.0
         self.HEIGHT = 2.0
         self.gPlotter = gPlotter
         self.tractionCircleModel = tractionCircleModel
-        self.colourFader = ColourFader(self.START_COLOUR, self.FINAL_COLOUR)
+        self.dataPointsColourFader = ColourFader(self.START_COLOUR_DATA_POINTS, self.FINAL_COLOUR_DATA_POINTS)
+        self.movingAverageColourFader = ColourFader(self.START_COLOUR_MOVING_AVERAGE, self.FINAL_COLOUR_MOVING_AVERAGE)
         self.movingAvgPlotter = movingAvgPlotter
 
     def drawScatterPlot(self, colourFades, dataPoints):
@@ -25,23 +28,27 @@ class TractionCircleView:
             ac.glQuad(x, y, self.WIDTH, self.HEIGHT)
         ac.glColor3f(1.0, 1.0, 1.0)
 
-    def drawMovingAverage(self, dataPoints):
+    def drawMovingAverage(self, colourFades, dataPoints):
         moving_average = self.movingAvgPlotter.plotMovingAverage(dataPoints)
         ac.glColor3f(1.0, 1.0, 1.0)
 
+        i = 0
         ac.glBegin(1)
         for point in moving_average:
+            ac.glColor3f(colourFades[i]['red'], colourFades[i]['green'], colourFades[i]['blue'])
             x, y = self.gPlotter.plotG(point['x'], point['z'])
+            i = i + 1
             ac.glVertex2f(x,y)
         ac.glEnd()
 
     def render(self):
         try:
             dataPoints = self.tractionCircleModel.dataPoints()
-            colourFades = self.colourFader.fade(len(dataPoints))
+            dataPointsColourFades = self.dataPointsColourFader.fade(len(dataPoints))
+            movingAverageColourFades = self.movingAverageColourFader.fade(len(dataPoints))
 
-            self.drawScatterPlot(colourFades, dataPoints)
-            self.drawMovingAverage(dataPoints)
+            self.drawScatterPlot(dataPointsColourFades, dataPoints)
+            self.drawMovingAverage(movingAverageColourFades, dataPoints)
 
         except Exception as e:
             ac.log(str(traceback.format_exc()))
