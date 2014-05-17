c5181919b2bf68ee5d2ae2aebc1f1be5456116f1
Bruno Gonzalez <stenyak@stenyak.com>
2014-05-17 14:29:17 +0100

    Add support for RGBA in colour_fader.

--- apps/python/traction-circle/traction_circle_view.py
+++ apps/python/traction-circle/traction_circle_view.py
@@ -6,10 +6,10 @@ from moving_average_plotter import MovingAveragePlotter
 
 
 class TractionCircleView:
-    FINAL_COLOUR_DATA_POINTS = {'red': 77, 'green': 184, 'blue': 77}
-    START_COLOUR_DATA_POINTS = {'red': 31, 'green': 77, 'blue': 31}
-    FINAL_COLOUR_MOVING_AVERAGE = {'red': 255, 'green': 255, 'blue': 255}
-    START_COLOUR_MOVING_AVERAGE = {'red': 64, 'green': 64, 'blue': 64}
+    FINAL_COLOUR_DATA_POINTS =    (0.25, 0.50, 0.10, 1.00)
+    START_COLOUR_DATA_POINTS =    (0.00, 0.75, 0.00, 0.00)
+    FINAL_COLOUR_MOVING_AVERAGE = (1.00, 1.00, 1.00, 1.00)
+    START_COLOUR_MOVING_AVERAGE = (0.75, 0.75, 0.75, 0.25)
 
     def __init__(self, window, tractionCircleModel, gPlotter, movingAvgPlotter):
         self.scatterSize = 0.04
@@ -59,14 +59,14 @@ class TractionCircleView:
 
     def drawScatterPlot(self, colourFades, dataPoints):
         for dataPoint, colour in zip(dataPoints, colourFades):
-            ac.glColor3f(colour['red'], colour['green'], colour['blue'])
+            ac.glColor4f(*colour)
             self.drawCircle(self.scatterSize, dataPoint)
         ac.glColor3f(1.0, 1.0, 1.0)
 
     def drawLinePlot(self, colourFades, dataPoints):
         ac.glBegin(1)
         for dataPoint, colour in zip(dataPoints, colourFades):
-            ac.glColor3f(colour['red'], colour['green'], colour['blue'])
+            ac.glColor4f(*colour)
             x, y = self.gPlotter.plotG(dataPoint['x'], dataPoint['z'])
             ac.glVertex2f(x,y)
         ac.glEnd()
