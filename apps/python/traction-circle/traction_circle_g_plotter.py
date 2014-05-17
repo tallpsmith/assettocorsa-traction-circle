b8053994ca98d3806e6e2307420ba7a5442fcf80
Bruno Gonzalez <stenyak@stenyak.com>
2014-05-17 13:38:39 +0100

    Maintain aspect ratio of G plots.

--- apps/python/traction-circle/traction_circle_g_plotter.py
+++ apps/python/traction-circle/traction_circle_g_plotter.py
@@ -2,6 +2,7 @@ class GPlotter():
     def __init__(self, windowSizeX, windowSizeY, maxXRange, maxZRange):
         self.windowSizeY = windowSizeY
         self.windowSizeX = windowSizeX
+        self.windowSize = min(windowSizeY, windowSizeX)
         self.maxZRange = maxZRange
         self.maxXRange = maxXRange
 
@@ -15,7 +16,7 @@ class GPlotter():
 
     def plotG(self, xGValue, zGValue):
         # normalize the acceleration to the Maximum G size, and offset that from the center point
-        xPoint = ((xGValue / self.maxXRange) * (0.5 * self.windowSizeX)) + (0.5 * self.windowSizeX)
-        yPoint = ((zGValue / self.maxZRange) * (0.5 * self.windowSizeY)) + (0.5 * self.windowSizeY)
+        xPoint = ((xGValue / self.maxXRange) * (0.5 * self.windowSize)) + (0.5 * self.windowSizeX)
+        yPoint = ((zGValue / self.maxZRange) * (0.5 * self.windowSize)) + (0.5 * self.windowSizeY)
 
         return xPoint, yPoint
